d3bdf4d6af17fdeb920db5c735c3411b0a6ee8af
Phong6698 <Phong@hotmail.ch>
2016-03-29 11:09:38 +0200

    buildpath

--- src/ch/grademasters/main/Main.java
+++ src/ch/grademasters/main/Main.java
@@ -1,12 +1,15 @@
 package ch.grademasters.main;
 
-import ch.grademasters.database.UserJDBCdao;
+import ch.grademasters.gui.Login;
 
 public class Main {
 
 	public static void main(String[] args) {
-		UserJDBCdao data = new UserJDBCdao();
-		data.registerUser("test", "test", "test", "test", "test");
+		new Login();
+//		UserJDBCdao data = new UserJDBCdao();
+//		data.registerUser("test", "test", "test", "test", "test");
+		
+		
 
 	}
 
